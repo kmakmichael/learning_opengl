620ad95c4019caeb8b85fbeaf30c5e204d159c0c
kmakmichael <m_kmak@u.pacific.edu>
2023-01-26 17:15:41 -0800

    glfw init

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 
 #include <GLFW/glfw3.h>
 
-int main(void)
-{
-    printf("test");  
+static void error_callback(int error, const char* desc) {
+    fprintf(stderr, "Error %s\n", desc);
+}
+
+int main(void) {
+    if (!glfwInit()) {
+        printf("initialization failed\n");
+        return -1;
+    }
+    printf("initialization successful\n");
+
+    glfwTerminate();
     return 0;
 }
